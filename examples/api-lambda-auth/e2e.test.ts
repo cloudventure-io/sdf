3beebbb159b10cab0fe0a19d747d497f25538ed1
Vahe Sahakyan <vahe@spir.me>
2023-11-04 22:51:34 +0100

    chore: fix example tests

--- examples/api-lambda-auth/e2e.test.ts
+++ examples/api-lambda-auth/e2e.test.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-import { OperationTestingResponses } from "./backend/interfaces"
+import { OperationIdentityMeResponses } from "./backend/.gen/interfaces"
 
 describe("end-to-end tests", () => {
   if (!process.env.SDF_TEST_API_URL) {
@@ -12,18 +12,18 @@ describe("end-to-end tests", () => {
   })
 
   it("basic", async () => {
-    const res = await client.get("/testing", { headers: { Authorization: "token" } })
+    const res = await client.get("/identity", { headers: { Authorization: "token" } })
 
     expect(res.status).toBe(200)
 
-    const body: OperationTestingResponses["body"] = res.data
+    const body: OperationIdentityMeResponses["body"] = res.data
 
     expect(body.ok).toBe(true)
     expect(body.user).toBe("token")
   })
 
   it("authorizer failed", async () => {
-    const res = await client.get("/testing", { headers: { Authorization: "t" }, validateStatus: () => true })
+    const res = await client.get("/identity", { headers: { Authorization: "t" }, validateStatus: () => true })
 
     expect(res.status).toBe(500)
   })
