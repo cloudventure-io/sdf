1c7ade8b5d1103ef578e48502a6c6a9f44a99325
Vahe Sahakyan <vahe@spir.me>
2025-04-19 22:22:02 +0200

    fix: JsonCodec type

--- src/http-api/runtime/codec/JsonCodec.ts
+++ src/http-api/runtime/codec/JsonCodec.ts
@@ -1,11 +1,11 @@
 import { Codec } from "./Codec"
 
-export class JsonCodec<Input> extends Codec<Input, string> {
+export class JsonCodec<Input> extends Codec<Input | undefined, string> {
   encode(input: Input): string {
     return input === undefined ? "" : JSON.stringify(input)
   }
 
-  decode(output: string): Input {
+  decode(output: string): Input | undefined {
     return output === "" ? undefined : JSON.parse(output)
   }
 }
